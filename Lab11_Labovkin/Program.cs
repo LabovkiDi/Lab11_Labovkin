1aad734880165c8b6bf959b550544ce4d6c4b2f4
Дмитрий Лабовкин <labovkindi@mail.ru>
2022-01-07 17:56:07 +0300

    Добавьте файлы проекта.

---
+++ Lab11_Labovkin/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11_Labovkin
+{
+    struct Equation
+    {
+        private const int zero = 0; /* неизменяемое поле*/
+        public double k;
+        public double b;
+        public Equation(double k, double b) /*создание конструктора структуры*/
+        {
+            this.k = k;
+            this.b = b;
+        }
+        //пишем метод решения линейного уравнение
+        public double Root()
+        {
+            return zero - b / k;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Equation equation = new Equation(-5.7, 13.4); /*создание экземляра струтуры*/
+            Console.WriteLine("Корень уравнения x равен");
+            Console.WriteLine(equation.Root());
+            Console.ReadKey();
+        }
+    }
+}
